575c247dd61dc79681fdcb304f93e2f14ec0c35c
Paul Ramsey <pramsey@cleverelephant.ca>
2021-08-16 12:11:30 -0700

    Allow urlencoding on bytea inputs. In general urlencode(text) will end up outputting encodings based on the server encoding (usually utf-8, but no guarantees). This change allows the urlencoded output to have a deterministic encoding by passing the text through convert_to() first to get an encoded binary string. Closes #124

--- http--1.3--1.4.sql
+++ http--1.3--1.4.sql
@@ -2,3 +2,9 @@ CREATE OR REPLACE FUNCTION http_list_curlopt ()
     RETURNS TABLE(curlopt text, value text)
     AS 'MODULE_PATHNAME', 'http_list_curlopt'
     LANGUAGE 'c';
+
+CREATE OR REPLACE FUNCTION urlencode(string BYTEA)
+    RETURNS TEXT
+    AS 'MODULE_PATHNAME'
+    LANGUAGE 'c'
+    IMMUTABLE STRICT;
