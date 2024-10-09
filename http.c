5e2bd270a9ce2b0e8e1fdf8e46b85396bd4125cd
Paul Ramsey <pramsey@cleverelephant.ca>
2024-10-09 13:11:32 -0700

    Version 1.6.1

--- http.c
+++ http.c
@@ -28,7 +28,7 @@
  ***********************************************************************/
 
 /* Constants */
-#define HTTP_VERSION "1.6.0"
+#define HTTP_VERSION "1.6.1"
 #define HTTP_ENCODING "gzip"
 #define CURL_MIN_VERSION 0x071400 /* 7.20.0 */
 
